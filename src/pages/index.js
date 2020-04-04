bcd34ea1717183e5536a88f659e09566640f8e28
iggy-pigy <ilgakokorisa@gmail.com>
2020-04-04 19:29:45 +0100

    Created geographic data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Helmet from "react-helmet";
 import L from "leaflet";
-import axios froms "axios";
+import axios from "axios";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
 
-
 const LOCATION = {
   lat: 38.9072,
-  lng: -77.0369
+  lng: -77.0369,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -22,25 +21,45 @@ const IndexPage = () => {
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement: map } = {
+  async function mapEffect({ leafletElement: map } = {}) {
     let response;
 
-
     try {
-      response = await axios.get('https://corona.lmao.ninja/countries');
-    } catch(e) {
+      response = await axios.get("https://corona.lmao.ninja/countries");
+    } catch (e) {
       console.log(`Failed to fetch countries: ${e.message}`, e);
       return;
     }
 
-    const { data = [] } = response;}
+    const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
 
+    if ( !hasData ) return;
+
+    const geoJson = {
+      type: ‘FeatureCollection’,
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: ‘Feature’,
+          properties: {
+          …country,
+          },
+          geometry: {
+          type: ‘Point’,
+          coordinates: [ lng, lat ]
+          }
+        }
+      })
+  }
+  }
 
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",
     zoom: DEFAULT_ZOOM,
-    mapEffect
+    mapEffect,
   };
 
   return (
